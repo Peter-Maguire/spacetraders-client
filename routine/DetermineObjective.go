45aef98ec1233333a7a5404d0d6d4ae7e99ea756
Peter Maguire <me@petermaguire.xyz>
2023-05-14 23:04:50 +0100

    wait for transit to finish before determining objective

--- routine/DetermineObjective.go
+++ routine/DetermineObjective.go
@@ -1,9 +1,20 @@
 package routine
 
+import "time"
+
 type DetermineObjective struct {
 }
 
 func (d DetermineObjective) Run(state *State) RoutineResult {
+
+	if state.Ship.Nav.Status == "IN_TRANSIT" && state.Ship.Nav.Route.Arrival.After(time.Now()) {
+		state.Log("We are currently going somewhere")
+		arrivalTime := state.Ship.Nav.Route.Arrival
+		return RoutineResult{
+			WaitUntil: &arrivalTime,
+		}
+	}
+
 	if state.Ship.IsMiningShip() {
 		if state.Ship.Cargo.Units >= state.Ship.Cargo.Capacity-5 {
 			state.Log("We're full up here")
