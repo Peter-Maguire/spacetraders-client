1b29c1ba879ab26961bd9ea4e6e57456d90630db
Peter Maguire <me@petermaguire.xyz>
2023-05-28 22:49:41 +0100

    add IsMineable helper function

---
+++ util/ItemProperties.go
@@ -0,0 +1,19 @@
+package util
+
+import "strings"
+
+// Items other than ores that can be mined
+var mineable = []string{"ICE_WATER", "QUARTZ_SAND", "AMMONIA_ICE", "SILICON_CRYSTALS"}
+
+func IsMineable(item string) bool {
+	if strings.HasSuffix(item, "_ORE") {
+		return true
+	}
+
+	for _, m := range mineable {
+		if m == item {
+			return true
+		}
+	}
+	return false
+}
