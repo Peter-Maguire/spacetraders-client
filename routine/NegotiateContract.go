18b1b558f1b53ef2026cd8824b36f584bbe17b45
Peter Maguire <me@petermaguire.xyz>
2023-05-29 14:34:28 +0100

    emit newContract event on contract negotiation

--- routine/NegotiateContract.go
+++ routine/NegotiateContract.go
@@ -9,15 +9,18 @@ func (n NegotiateContract) Run(state *State) RoutineResult {
 	_ = state.Ship.EnsureNavState(entity.NavDocked)
 	newContract, err := state.Ship.NegotiateContract()
 
+	// TODO: contract logic
+
 	if err == nil {
 		state.Log("New contract get")
 		_ = newContract.Accept()
 		state.Contract = newContract
+		state.FireEvent("newContract", newContract)
 	} else {
 		state.Log(err.Error())
 	}
 
-	return RoutineResult{SetRoutine: DetermineObjective{}}
+	return RoutineResult{SetRoutine: DetermineObjective{}, WaitSeconds: 5}
 }
 
 func (n NegotiateContract) Name() string {
