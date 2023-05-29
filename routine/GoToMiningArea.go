5a9870bd758de9e904f74e41babce80649883051
Peter Maguire <me@petermaguire.xyz>
2023-05-29 16:42:09 +0100

    Go to best mining area available in this system rather than the first asteroid field

--- routine/GoToMiningArea.go
+++ routine/GoToMiningArea.go
@@ -1,49 +1,93 @@
 package routine
 
-import "spacetraders/entity"
+import (
+	"fmt"
+	"sort"
+	"spacetraders/entity"
+)
 
 type GoToMiningArea struct {
-    next Routine
+	next Routine
 }
 
 func (g GoToMiningArea) Run(state *State) RoutineResult {
-    waypointData, _ := state.Ship.Nav.WaypointSymbol.GetWaypointData()
+	_ = state.Ship.EnsureNavState(entity.NavOrbit)
 
-    // We are currently in an asteroid field
-    if waypointData.Type == "ASTEROID_FIELD" {
-        state.Log("We are already in an asteroid field, convenient!")
-        return RoutineResult{
-            SetRoutine: g.next,
-        }
-    }
+	waypointsPtr, _ := state.Ship.Nav.WaypointSymbol.GetSystemWaypoints()
+	waypoints := *waypointsPtr
+	waypointScores := make(map[entity.Waypoint]int)
 
-    _ = state.Ship.EnsureNavState(entity.NavOrbit)
+	for _, waypoint := range waypoints {
+		waypointScores[waypoint.Symbol] = g.ScoreWaypoint(waypoint)
+	}
 
-    system, _ := state.Ship.Nav.WaypointSymbol.GetSystem()
+	sort.Slice(waypoints, func(i, j int) bool {
+		return waypointScores[waypoints[i].Symbol] > waypointScores[waypoints[j].Symbol]
+	})
 
-    for _, waypoint := range system.Waypoints {
-        if waypoint.Type == "ASTEROID_FIELD" {
-            return RoutineResult{
-                SetRoutine: NavigateTo{waypoint.Symbol, g.next},
-            }
-        }
-    }
+	bestWaypoint := waypoints[0]
 
-    if state.Ship.Nav.SystemSymbol != state.Agent.Headquarters.GetSystemName() {
-        return RoutineResult{
-            SetRoutine: GoToJumpGate{next: GoToSystem{
-                system: state.Agent.Headquarters.GetSystemName(),
-                next:   g,
-            }},
-        }
-    }
+	state.Log(fmt.Sprintf("Waypoint %s has score of %d", bestWaypoint.Symbol, waypointScores[bestWaypoint.Symbol]))
 
-    state.Log("Couldn't find a waypoint pointing to an asteroid field")
-    return RoutineResult{
-        WaitSeconds: 60,
-    }
+	if waypointScores[bestWaypoint.Symbol] > 0 {
+		return RoutineResult{SetRoutine: NavigateTo{
+			waypoint: bestWaypoint.Symbol,
+			next:     MineOres{},
+		}}
+	}
+
+	if state.Ship.Nav.SystemSymbol != state.Agent.Headquarters.GetSystemName() {
+		return RoutineResult{
+			SetRoutine: GoToJumpGate{next: GoToSystem{
+				system: state.Agent.Headquarters.GetSystemName(),
+				next:   g,
+			}},
+		}
+	}
+
+	state.Log("Couldn't find a waypoint pointing to an asteroid field")
+	return RoutineResult{
+		WaitSeconds: 60,
+	}
+}
+
+func (g GoToMiningArea) ScoreWaypoint(waypoint entity.WaypointData) int {
+	score := 0
+	if waypoint.HasTrait("PRECIOUS_METAL_DEPOSITS") {
+		score += 25
+	}
+
+	if waypoint.HasTrait("RARE_METAL_DEPOSITS") {
+		score += 20
+	}
+
+	if waypoint.HasTrait("COMMON_METAL_DEPOSITS") {
+		score += 20
+	}
+
+	if waypoint.HasTrait("MINERAL_DEPOSITS") {
+		score += 5
+	}
+
+	if waypoint.HasTrait("MARKETPLACE") {
+		score += 2
+	}
+
+	if waypoint.HasTrait("OVERCROWDED") {
+		score -= 5
+	}
+
+	if waypoint.HasTrait("BARREN") {
+		score -= 10
+	}
+
+	if waypoint.HasTrait("STRIPPED") {
+		score -= 20
+	}
+
+	return score
 }
 
 func (g GoToMiningArea) Name() string {
-    return "Go To Asteroid Field"
+	return "Go To Mining Area"
 }
